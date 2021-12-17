499c5b68c523bb992c4a26bac02eea0f094f429e
mnixdorf <35327832+mnixdorf@users.noreply.github.com>
2021-12-17 13:07:28 +0100

    structure + threads, event

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # IDE / SHELL needs to be run as Admin to work!
+from datetime import datetime
 import threading
 
 import PySimpleGUI as pg
 import pyautogui
 import time
 import win32gui
+import sys
 
 interval = 0
 gui_done = False
 WAIT = 2
 i = 0
+stop = False
 
 def build_gui():
     window = pg.Window('App', layout=[[pg.Text('Insert New Application Name: ', size=(25, 1)),
@@ -23,35 +26,45 @@ def build_gui():
                                       [pg.Button(button_text='Start', key='btn_start')]], margins=(200, 200)).read()
     return window
 
-def run(hwnd, hwnd_old, interval, repetitions):
-    i = 0
-    print(interval)
-    while i < repetitions:
-        i += 1
-        win32gui.SetForegroundWindow(hwnd)
-        threading.Timer(WAIT, pyautogui.keyDown('w'))
-        time.sleep(WAIT)
-        threading.Timer(WAIT, pyautogui.keyUp('w'))
-        threading.Timer(WAIT, win32gui.SetForegroundWindow(hwnd_old))
-        threading.Timer(interval * 1000, print(i))
-        print("done")
+def build_confirm():
+    window_close = pg.Window('Confirm', layout=[[pg.Button(button_text='Stop', key='btn_stop')]], margins=(30,30)).read()
+    return window_close
 
-def btn_start():
+
+def run(hwnd, hwnd_old, interval, event):
+    print(interval)
+    while not stop:
+        event.wait(2)
+        win32gui.SetForegroundWindow(hwnd)
+        event.wait(2)
+        pyautogui.keyDown('w')
+        event.wait(5)
+        pyautogui.keyUp('w')
+        event.wait(2)
+        win32gui.SetForegroundWindow(hwnd_old)
+        event.wait(interval)
+
+def btn_start(e):
+    print(threading.enumerate())
     hwnd = win32gui.FindWindowEx(0, 0, 0, value['application_name_new'])
     hwnd_old = win32gui.FindWindowEx(0, 0, 0, value['application_name_old'])
     interval = float(value['duration']) * 60
-    repetitions = 5
-    worker = threading.Thread(target=run, args=(hwnd, hwnd_old, interval, repetitions))
-    print('reached')
-    worker.start()
+    run(hwnd, hwnd_old, interval, e)
 
 
-window = build_gui()
-while True:
-    event, value = window
-    if event == 'btn_start':
-        btn_start()
-        break
-print("done")
-# hwnd = win32gui.FindWindowEx(0,0,0, 'FINAL FANTASY XIV')
-# hwnd_old = win32gui.FindWindowEx(0,0,0, 'App')
+if __name__ == '__main__':
+    e = threading.Event()
+    window = build_gui()
+    while True:
+        event, value = window
+        if event == 'btn_start':
+            print(threading.enumerate())
+            threading.Thread(target=btn_start, args=(e,)).start()
+            break
+    window_end = build_confirm()
+    while True:
+        event, value = window_end
+        if event == 'btn_stop':
+            e.set()
+            stop = True
+            sys.exit()
